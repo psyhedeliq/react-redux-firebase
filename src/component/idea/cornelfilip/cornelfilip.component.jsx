8e7eab5b10dba5e2d350c456593d95a7432b1214
psyhedeliq <filipcornelcristian@gmail.com>
2020-04-04 01:59:37 +0100

    Revert "implement useReducer, read from the database and display some data"
    This reverts commit 73a006934ebb6d25693ce613e3d2c6abfea68031.

--- src/component/idea/cornelfilip/cornelfilip.component.jsx
+++ src/component/idea/cornelfilip/cornelfilip.component.jsx
@@ -1,22 +1,64 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { db } from './firestore';
 
+const initialSubjectData = [];
+
+const subjectReducer = (state = initialSubjectData, action) => {
+  switch (action.type) {
+    case 'INIT_SUBJECT_LIST':
+      console.log('##############################');
+      console.log(state, action);
+      return action.data;
+    default:
+      return state;
+  }
+};
+
 const CornelFilip = () => {
+  const [subjectData, updateSubjectData /* dispatch */] = useReducer(
+    subjectReducer,
+    initialSubjectData
+  );
+
+  const subjectCollection = db.collection('subject');
+
   useEffect(() => {
     (async () => {
       // connect to the firestore
-      db.collection('subject').add({
-        title: 'Firestore database with React, Redux and ES6+',
-        description:
-          'Having some fun, connecting and interacting with a database',
-        createdAt: +new Date()
-      });
+      try {
+        const subjectList = await subjectCollection.get();
+        const data = subjectList.docs.map(doc => {
+          return { id: doc.id, ...doc.data() };
+        });
+        updateSubjectData({
+          type: 'INIT_SUBJECT_LIST',
+          data
+        });
+      } catch (error) {
+        console.log('Getting a list from firestore FAILED!', error);
+      }
     })();
   }, []);
 
+  const addSubject = () => {
+    subjectCollection.add({
+      title: 'Firestore database with React, Redux and ES6+',
+      description:
+        'Having some fun, connecting and interacting with a database',
+      createdAt: +new Date()
+    });
+  };
+
   return (
     <div>
-      <h1>Cornel Filip</h1>
+      <h1>Subjects to learn and discover</h1>
+      <div>
+        {/* we destructure id and title from subject */}
+        {subjectData.map(({ id, title }, key) => {
+          // console.log(subject);
+          return <div key={id}>{title}</div>;
+        })}
+      </div>
     </div>
   );
 };
